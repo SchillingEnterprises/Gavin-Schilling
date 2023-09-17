a3499f20ee362073d242d199a48d2abe1e80d983
Gavin Schilling <schillgc@hotmail.com>
2023-09-17 14:48:22 -0400

    Initial Commit

--- static/js/mojo.js
+++
@@ -1,8 +0,0 @@
-require(["mojo/signup-forms/Loader"],
-    function (L) {
-        L.start({
-            "baseUrl": "mc.us14.list-manage.com"
-            , "uuid": "c7e6ac211ba61e50f8ddf1686"
-            , "lid": "15dc97ed11"
-        })
-    })
