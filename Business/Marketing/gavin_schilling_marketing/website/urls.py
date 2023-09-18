9f0d8a94ada6bc7adf67bbd4c07281ac12e13c41
Gavin Schilling <schillgc@hotmail.com>
2023-09-17 23:39:11 -0400

    Add for static

--- Business/Marketing/gavin_schilling_marketing/website/urls.py
+++ Business/Marketing/gavin_schilling_marketing/website/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -10,4 +12,4 @@ urlpatterns = [
     path('information_technology/network_engineering/', views.network_engineering, name='network_engineering'),
     path('information_technology/web_development/', views.web_development, name='web_development'),
     path('strategic_marketing/buzz_marketing/', views.buzz_marketing_service, name='buzz_marketing'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
