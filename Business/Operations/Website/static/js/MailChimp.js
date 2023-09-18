fbdb62d78ce0ef6ce4b2997fdd6d3142b024f72a
Gavin Schilling <schillgc@hotmail.com>
2023-09-17 20:07:28 -0400

    Initial Commit

---
+++ Business/Operations/Website/static/js/MailChimp.js
@@ -0,0 +1,8 @@
+! function (c, h, i, m, p) {
+    m = c.createElement(h), p = c
+        .getElementsByTagName(h)[0], m
+        .async = 1, m.src = i, p
+        .parentNode.insertBefore(m, p)
+}(document, "script",
+    "https://chimpstatic.com/mcjs-connected/js/users/c7e6ac211ba61e50f8ddf1686/5f47cc339ad1153486c5ebe31.js"
+    );
