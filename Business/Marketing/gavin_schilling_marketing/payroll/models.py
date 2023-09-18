9ac45a194c04863db86f5ae3736f83b1408b9042
Gavin Schilling <schillgc@hotmail.com>
2023-09-17 23:36:57 -0400

    Add TimeSheet, and other models from website

--- Business/Marketing/gavin_schilling_marketing/payroll/models.py
+++ Business/Marketing/gavin_schilling_marketing/payroll/models.py
@@ -1,24 +1,33 @@
+from django.apps import AppConfig
 from django.db import models
+from django.db.models.signals import pre_save
 from djmoney.models.fields import MoneyField
 from djmoney.models.validators import MinMoneyValidator
-from website.models import Client, Company, Employee
+
+class WebsiteConfig(AppConfig):
+    def ready(self):
+        from ..website.models import Employee, Company, Client
+
+        Employee = self.get_model("Employee")
+        Company = self.get_model("Company")
+        Client = self.get_model("Client")
 
 
-class TimeSheet(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    contractor = models.ForeignKey(Company, on_delete=models.CASCADE, blank=True)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True)
+        class TimeSheet(models.Model):
+            employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+            contractor = models.ForeignKey(Company, on_delete=models.CASCADE, blank=True)
+            client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True)
 
-    pay_rate = MoneyField(
-        max_digits=6,
-        decimal_places=2,
-        default_currency='USD',
-        validators=[
-            MinMoneyValidator({'USD': 0.01})
-        ],
-    )
+            pay_rate = MoneyField(
+                max_digits=6,
+                decimal_places=2,
+                default_currency='USD',
+                validators=[
+                    MinMoneyValidator({'USD': 0.01})
+                ],
+            )
 
-    work_duration = models.DecimalField(name='Duration of Work', max_digits=6, decimal_places=3)
+            work_duration = models.DecimalField(name='Duration of Work', max_digits=6, decimal_places=3)
 
 
 class TaxRate (models.Model):
